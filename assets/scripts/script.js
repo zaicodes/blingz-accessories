6d4f151367eb8f27617fafd9c79798f8af1b47e6
Zainab <zinabsamater@hotmail.co.uk>
2023-12-26 22:29:26 +0000

    Add: sign up page, functionality and changed pictures

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,23 @@
 const menu = document.querySelector(".menu");
+const user = document.querySelector(".user-logo");
+const closelogin = document.querySelector(".close-login");
+const login = document.querySelector(".login");
+const signinbtn = document.querySelector(".signin-btn");
 menu.addEventListener("click", () => {
   console.log("clciked");
   document.querySelector(".nav-links").classList.toggle("navshow");
+});
+
+function toggleloginform() {
+  document.querySelector(".login").classList.toggle("wrapper");
+}
+
+user.addEventListener("click", () => {
+  toggleloginform();
+});
+closelogin.addEventListener("click", () => {
+  toggleloginform();
+});
+signinbtn.addEventListener("click", () => {
+  toggleloginform();
 });
