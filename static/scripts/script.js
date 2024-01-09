ac494e414537e2df4b8d6154b5fe98c5852f9ebf
Zainab <zinabsamater@hotmail.co.uk>
2024-01-09 22:11:08 +0000

    Added "Your Cart" section to base.html

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,6 +3,30 @@ const user = document.querySelector(".user-logo");
 const closelogin = document.querySelector(".close-login");
 const login = document.querySelector(".login");
 const signinbtn = document.querySelector(".signin-btn");
+const cart = document.querySelector(".cart");
+const closingbtn = document.querySelector(".closing-btn");
+const cartmain = document.querySelector(".cart-main");
+const wishlistbtn = document.querySelector(".wishlistbtn");
+const showcartbtns = document.querySelectorAll(".showcartmain");
+
+// for showing cart section
+const showcartmain = function () {
+  cartmain.classList.add("wrapper");
+};
+showcartbtns.forEach(function (e) {
+  e.addEventListener("click", function () {
+    showcartmain();
+  });
+});
+
+// for hiding the the cart
+const hidecartmain = function () {
+  cartmain.classList.remove("wrapper");
+};
+
+closingbtn.addEventListener("click", hidecartmain);
+
+// for the slide of navbar
 menu.addEventListener("click", () => {
   console.log("clciked");
   document.querySelector(".nav-links").classList.toggle("navshow");
@@ -12,6 +36,7 @@ function toggleloginform() {
   document.querySelector(".login").classList.toggle("wrapper");
 }
 
+// for the login form
 user.addEventListener("click", () => {
   toggleloginform();
 });
