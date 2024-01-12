937f11960a1b5f1b806d014f4f3ea0b044076a79
Zainab <zinabsamater@hotmail.co.uk>
2024-01-12 13:23:24 +0000

    Fix: error with items total not adding up correctly

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -9,6 +9,7 @@ const cartmain = document.querySelector(".cart-main");
 const wishlistbtn = document.querySelector(".wishlist-btn");
 const showcartbtns = document.querySelectorAll(".showcartmain");
 const emptypara = document.querySelector(".emty-para");
+
 // for showing cart section
 const showcartmain = function () {
   cartmain.classList.add("wrapper");
@@ -106,18 +107,18 @@ $(document).ready(function () {
 
     // Create a new cart item
     var cartItem = `
-      <div class="cart-flex">
-        <div>
-          <img src="${imgSrc}" alt="${name}" />
-        </div>
-        <div class="cart-info">
-          <h2 class="small-heading">${name}</h2>
-          <p class="description_para">${price}</p>
-          <i class="fa-solid fa-trash removebtn" aria-label="Removing-button" title="Remove item"></i>
-        </div>
-        <input type="number" aria-label="Update quantity" title="Update quantity" id="" />
-      </div>
-    `;
+  <div class="cart-flex">
+    <div>
+      <img src="${imgSrc}" alt="${name}" />
+    </div>
+    <div class="cart-info">
+      <h2 class="small-heading">${name}</h2>
+      <p class="description_para">${price}</p>
+      <i class="fa-solid fa-trash removebtn" aria-label="Removing-button" title="Remove item"></i>
+    </div>
+    <input type="number" aria-label="Update quantity" title="Update quantity" id="" />
+  </div>
+`;
 
     // Append the cart item to the cart grid
     $(".cart-grid").append(cartItem);
@@ -139,6 +140,11 @@ document.addEventListener("click", function (event) {
 
     // Check if the cart item exists before attempting to remove it
     if (cartItem) {
+      // Get the price of the item being removed
+      const removedPrice = parseFloat(
+        cartItem.querySelector(".description_para").textContent.replace("£", "")
+      );
+
       // Remove the cart item
       cartItem.remove();
 
@@ -149,8 +155,17 @@ document.addEventListener("click", function (event) {
         $(".checkoutbtn").addClass("hidden");
         // Show the empty cart message
         $(".emty-para").show();
+        $(".totalprice div:last-child").text(`£ 0`);
+
         // Hide the cart grid
         $(".cart-grid").addClass("hidden");
+      } else {
+        // Update the total price by subtracting the removed item's price
+        var totalPrice =
+          parseFloat($(".totalprice div:last-child").text().replace("£", "")) -
+          removedPrice;
+        $(".totalprice div:last-child").text(`£ ${totalPrice}`);
+        console.log(totalPrice);
       }
     }
   }
